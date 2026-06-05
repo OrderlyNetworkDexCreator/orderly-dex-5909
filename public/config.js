43d91ad854b7ace354b69f9a52afe0c40a9ffde5
orderly-guoqing <guoqing@orderly.network>
2026-06-06 05:53:05 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "https://x.com/orderlynetwork",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://vrfy-poc.ia01.com.br",
+  "VITE_SEO_SITE_URL": "https://dex.orderly.network/orderly-dex-5909/",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
